2240cfb19565ecaa0d1b8b8a2c8208846e12552c
HadeyRragheb <hade.399r@gmail.com>
2025-06-08 15:54:47 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿EXEC sp_attach_db
+    @dbname = N'housing100',
+    @filename1 = N'C:\Users\W10\Desktop\CRMProject\CRMProject\DB\housing100.mdf';
---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿-- attach the existing MDF as logical database "housing100"
+CREATE DATABASE housing100
+ON  ( FILENAME = N'C:\Users\W10\Desktop\CRMProject\CRMProject\DB\housing100.mdf' )
+FOR ATTACH;
+GO
+
+CREATE DATABASE housing100
+ON
+  ( FILENAME = N'C:\...\housing100.mdf' ),
+  ( FILENAME = N'C:\...\housing100_log.ldf' )
+FOR ATTACH;
